5931673f258b1ad7fa533f78f21c8aca5e737d56
Alexandr Fomenko <fomenko954@gmail.com>
2016-02-02 16:18:03 +0200

    server for android application

---
+++ src/main/java/ua/nure/server/config/WebConfig.java
@@ -0,0 +1,13 @@
+package ua.nure.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("ua.nure.server")
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+}
